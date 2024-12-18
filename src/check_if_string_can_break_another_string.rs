026226325e7435da6ee5ecca72e9ad5f61fe966f
Daniel Lopez <danielxdad@gmail.com>
2024-12-18 13:00:40 -0500

    implementing solution of the problem 1433 in rust

---
+++ src/check_if_string_can_break_another_string.rs
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/check-if-a-string-can-break-another-string/description/
+
+pub fn solution(s1: String, s2: String) -> bool {
+    let mut s1_vec = Vec::from(s1.as_str());
+    let mut s2_vec = Vec::from(s2.as_str());
+    let mut s1_small = true;
+    let mut s2_small = true;
+
+    s1_vec.sort_unstable();
+    s2_vec.sort_unstable();
+
+    for i in 0..s1_vec.len() {
+        let cs1 = s1_vec[i];
+        let cs2 = s2_vec[i];
+
+        s1_small &= cs1 <= cs2;
+        s2_small &= cs2 <= cs1;
+    }
+
+    return s1_small || s2_small;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod check_if_string_can_break_another_string;
 #[allow(dead_code)]
 mod elimination_game;
 mod longest_palindrome;
@@ -5,7 +6,56 @@ mod make_array_strictly_increasing;
 mod string_to_integer_atoi;
 
 fn main() {
-    println!("Run: cargo test");
+    // println!("Run: cargo test");
+
+    for (s1, s2) in [
+        ("abc", "xya"),
+        ("abe", "acd"),
+        ("leetcodee", "interview"),
+        ("mgldgsvnsgpdvm", "jqpaktmjafgkzs"),
+    ] {
+        println!(
+            "s1=\"{}\", s2=\"{}\": {:?}",
+            s1,
+            s2,
+            check_if_string_can_break_another_string::solution(String::from(s1), String::from(s2))
+        );
+    }
 
     // println!("{}", string_to_integer_atoi::my_atoi_1("42".to_string()));
+
+    //let case = 0;
+    //if case == 0 {
+    //    println!(
+    //        "Result: {}",
+    //        make_array_strictly_increasing::make_array_increasing_v5(
+    //            [1, 5, 3, 6, 7].to_vec(),
+    //            [1, 3, 2, 4].to_vec()
+    //        )
+    //    );
+    //} else if case == 1 {
+    //    println!(
+    //        "{}",
+    //        make_array_strictly_increasing::make_array_increasing_v3(
+    //            [1, 5, 3, 6, 7].to_vec(),
+    //            [1, 6, 3, 3].to_vec()
+    //        )
+    //    );
+    //} else if case == 2 {
+    //    println!(
+    //        "{}",
+    //        make_array_strictly_increasing::make_array_increasing_v3(
+    //            [0, 11, 6, 1, 4, 3].to_vec(),
+    //            [5, 4, 11, 10, 1, 0].to_vec()
+    //        )
+    //    );
+    //} else if case == 3 {
+    //    println!(
+    //        "{}",
+    //        make_array_strictly_increasing::make_array_increasing_v3(
+    //            [5, 16, 19, 2, 1, 12, 7, 14, 5, 16].to_vec(),
+    //            [6, 17, 4, 3, 6, 13, 4, 3, 18, 17, 16, 7, 14, 1, 16].to_vec(),
+    //        )
+    //    );
+    //}
 }
